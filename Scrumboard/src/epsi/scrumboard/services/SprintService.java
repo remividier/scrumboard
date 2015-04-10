dec68301297004d89b06320bf0d01e804971e57b
rvidier <remi.vidier@epsi.fr>
2015-04-10 15:34:08 +0200

    add file

---
+++ Scrumboard/src/epsi/scrumboard/services/SprintService.java
@@ -0,0 +1,14 @@
+package epsi.scrumboard.services;
+
+import epsi.scrumboard.beans.Project;
+
+/**
+ * Created by eklektik on 10/04/2015.
+ */
+public interface SprintService {
+
+
+    void addProject (Project project);
+
+
+}
