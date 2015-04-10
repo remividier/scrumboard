78ba1779132aba0927fe30a37d37dfbb3298d710
rvidier <remi.vidier@epsi.fr>
2015-04-10 17:00:30 +0200

    add functions

--- Scrumboard/src/epsi/scrumboard/services/SprintService.java
+++ Scrumboard/src/epsi/scrumboard/services/SprintService.java
@@ -1,15 +1,15 @@
 package epsi.scrumboard.services;
 
 import epsi.scrumboard.beans.Sprint;
-import epsi.scrumboard.beans.Task;
 
-import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by eklektik on 10/04/2015.
  */
 public interface SprintService {
 
+    void addSprint(Sprint sprint);
 
-
+    List<Sprint> getSprints();
 }
--- Scrumboard/src/epsi/scrumboard/services/impl/SprintServiceImpl.java
+++ Scrumboard/src/epsi/scrumboard/services/impl/SprintServiceImpl.java
@@ -1,11 +1,39 @@
 package epsi.scrumboard.services.impl;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import org.bson.types.ObjectId;
+import epsi.scrumboard.beans.Sprint;
+import epsi.scrumboard.mongo.Connection;
 import epsi.scrumboard.services.SprintService;
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * Created by Pierre on 10/04/2015.
  */
 public class SprintServiceImpl implements SprintService {
 
+    @Override
+    public void addSprint(int idProject, Sprint sprint) {
+
+        DBObject sprinttForDB = new BasicDBObject();
+
+        if(sprinttForDB.get("sprint") == null){
+            sprinttForDB.put("name",sprint.getName());
+            sprinttForDB.put("sprint",sprint.getName());
+        } else {
+            sprinttForDB.put("name",sprint.getName());
+        }
+        sprinttForDB.put("name",sprint.getName());
+        sprinttForDB.put("dod", sprint.getDod());
+
+        Connection.insert(sprinttForDB, "sprints");
+
+    }
 
 }
+
+
+
